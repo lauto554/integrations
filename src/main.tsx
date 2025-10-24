ce9790a65789ab0f5fb9c174300f65a97e8ef300
lauto554 <lautarojm185@gmail.com>
2025-10-24 15:21:58 -0300

    agrego query client

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,14 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import Router from "./router";
 import "./assets/style.css";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <Router />
+    <QueryClientProvider client={queryClient}>
+      <Router />
+    </QueryClientProvider>
   </React.StrictMode>
 );
