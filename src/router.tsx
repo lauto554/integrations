5ab38a169c40051bd8e3a397c175b38002be6e5e
lauto554 <lautarojm185@gmail.com>
2025-10-24 15:22:06 -0300

    agrego protyected route

--- src/router.tsx
+++ src/router.tsx
@@ -2,14 +2,19 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Layout from "./views/Layout";
 import MercadoPagoView from "./views/app/mercadopago/MercadoPagoView";
 import HomeView from "./views/app/home/HomeView";
+import PortalView from "./views/auth/PortalView";
+import ProtectedRoute from "./views/auth/_components/ProtectedRoute";
 
 export default function Router() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<HomeView />} />
-          <Route path="mercado-pago" element={<MercadoPagoView />} />
+        <Route path="/" element={<PortalView />} />
+        <Route element={<ProtectedRoute />}>
+          <Route path="/inicio" element={<Layout />}>
+            <Route index element={<HomeView />} />
+            <Route path="mercado-pago" element={<MercadoPagoView />} />
+          </Route>
         </Route>
       </Routes>
     </BrowserRouter>
